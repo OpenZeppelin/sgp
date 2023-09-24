3955e5c8afc73306d75dece0c701e1149296d614
George Plotnikov <accembler@gmail.com>
2023-09-24 20:21:44 +0500

    fix lint error

--- src/tokens.py
+++ src/tokens.py
@@ -59,7 +59,7 @@ def get_token_type_map(tokens: str) -> Dict[int, str]:
 
 #TODO: what does this do?
 def build_token_list(tokens_arg: List[Dict[str, Any]], options: Dict[str, Any]) -> List[Dict[str, Any]]:
-    token_types = get_token_type_map(tokens)
+    token_types = get_token_type_map(tokens_arg)
     result = []
 
     for token in tokens_arg:
