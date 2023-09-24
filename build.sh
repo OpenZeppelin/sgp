3d1d231a98bc67166126bd9b648dd1c213c1c111
Georgii Plotnikov <accembler@gmail.com>
2023-09-24 13:53:01 +0500

    Add strict antlr4 version build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 ANTLR_JAR="antlr4.jar"
-GRAMMAR="Solidity"
 
 if ! command -v python3 &> /dev/null
 then
   apt install python3
   apt install python3-pip
-  pip install antlr4-python3-runtime
+  pip install antlr4-python3-runtime==4.13.1
 fi
 
 if ! command -v java &> /dev/null
@@ -20,5 +19,5 @@ if [ ! -e "$ANTLR_JAR" ]; then
   curl https://www.antlr.org/download/antlr-4.13.1-complete.jar -o "$ANTLR_JAR"
 fi
 
-java -jar $ANTLR_JAR -Dlanguage=Python3 $GRAMMAR.g4 -visitor -o out/
+java -jar $ANTLR_JAR -Dlanguage=Python3 Solidity.g4 -visitor -o out/
 
