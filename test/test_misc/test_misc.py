06555a70e70ea11fa347ceb439f3099c8ea8e5b8
Georgii Plotnikov <accembler@gmail.com>
2024-02-02 09:37:22 +0800

    rollback `TestMisc`

--- test/test_misc/test_misc.py
+++ test/test_misc/test_misc.py
@@ -5,9 +5,7 @@ from sgp.sgp_parser import parse
 
 class TestMisc(unittest.TestCase):
     def test_misc(self) -> None:
-        input = """function test() public pure returns(address payable) {
-        return payable(address(0));
-    }"""
+        input = """"""
 
         ast = parse(input)
         self.assertIsNotNone(ast)
