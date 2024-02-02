0b0218f00e4c802a362d6628d81c39820a32d401
Georgii Plotnikov <accembler@gmail.com>
2024-02-02 16:12:17 +0800

    add unit test

--- test/test_misc/test_function_returns_address_payable.py
+++ test/test_misc/test_function_returns_address_payable.py
@@ -23,3 +23,23 @@ class TestFunctionReturnsAddressPayable(unittest.TestCase):
         self.assertEqual(
             "payable", ast.children[0].return_parameters[0].type_name.state_mutability
         )
+
+    def test_function_returns_address(self) -> None:
+        input = """function test() public returns(address) {}"""
+        ast = parse(input)
+        self.assertIsNotNone(ast)
+        self.assertEqual(1, len(ast.children))
+        self.assertEqual("FunctionDefinition", ast.children[0].type)
+        self.assertEqual(1, len(ast.children[0].return_parameters))
+        self.assertEqual(
+            "VariableDeclaration", ast.children[0].return_parameters[0].type
+        )
+        self.assertIsNone(ast.children[0].return_parameters[0].name)
+        self.assertIsNotNone(ast.children[0].return_parameters[0].type_name)
+        self.assertEqual(
+            "ElementaryTypeName", ast.children[0].return_parameters[0].type_name.type
+        )
+        self.assertEqual("address", ast.children[0].return_parameters[0].type_name.name)
+        self.assertIsNone(
+            ast.children[0].return_parameters[0].type_name.state_mutability
+        )
--- test/test_misc/test_misc.py
+++ test/test_misc/test_misc.py
@@ -5,7 +5,7 @@ from sgp.sgp_parser import parse
 
 class TestMisc(unittest.TestCase):
     def test_misc(self) -> None:
-        input = """"""
+        input = """add_your_solidity_code_here"""
 
         ast = parse(input)
         self.assertIsNotNone(ast)
